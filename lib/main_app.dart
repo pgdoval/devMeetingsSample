e57ed708827bd048375b9bc80775a2af18e0402c
Pablo González Doval <dovaleac@gmail.com>
2016-08-22 21:31:27 +0200

    First sample element, the web is running!

---
+++ lib/main_app.dart
@@ -0,0 +1,17 @@
+//18/
+@HtmlImport('main_app.html')
+library dev_meetings_sample.main_app;
+
+import 'dart:html';
+
+import 'package:polymer/polymer.dart';
+import 'package:web_components/web_components.dart';
+
+// id del elemento
+@PolymerRegister('main-app')
+class MainApp extends PolymerElement {
+
+  MainApp.created() : super.created();
+
+
+}
