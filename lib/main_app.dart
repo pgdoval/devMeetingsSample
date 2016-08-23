88dbb707c8ce390cc466ff2c659083a54128aa3c
Pablo González Doval <dovaleac@gmail.com>
2016-08-23 19:18:44 +0200

    Importing some paper elements

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,5 +1,4 @@
-// Copyright (c) 2016, Pablo González Doval. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+
 @HtmlImport('main_app.html')
 library dev_meetings_sample.main_app;
 
@@ -11,7 +10,6 @@ import 'package:web_components/web_components.dart';
 @PolymerRegister('main-app')
 class MainApp extends PolymerElement {
 
-  /// Constructor used to create instance of MainApp.
   MainApp.created() : super.created();
 
 
--- web/index.dart
+++ web/index.dart
@@ -1,11 +1,10 @@
-// Copyright (c) 2016, Pablo González Doval. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 
 library dev_meetings_sample.index;
 
 import 'package:dev_meetings_sample/main_app.dart';
 import 'package:polymer/polymer.dart';
+// necessary for using polymer made elements
+export 'package:polymer/init.dart';
 
 /// [MainApp] used!
 main() async {
